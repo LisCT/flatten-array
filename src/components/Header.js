086b3a549c0af3e02da9e3a0984ce636c2d21109
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-10-25 18:21:54 -0500

    adding stating files

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>Flat your Arrays</h1>
+        <p> This app will flatten an array of arbitrarily nested arrays of integers into a flat array of integers. e.g. [[1,2,[3]],4] -> [1,2,3,4]</p>
+    </header>
+)
+
+export default Header;
