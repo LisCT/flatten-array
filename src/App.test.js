417223fae69542ca18244c78500aad566bdbbac5
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-10-29 13:53:07 -0500

    spaces

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { convertToFlatArray, convertToArray } from './App';
+
+import App from './App';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 
