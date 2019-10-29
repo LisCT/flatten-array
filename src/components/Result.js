a4b6e24ea4bed13bdb695e589a11cf1abdc0f384
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-10-29 15:26:00 -0500

    adding (CI)

--- src/components/Result.js
+++ src/components/Result.js
@@ -17,7 +17,7 @@ const Result =({value, solution, explain}) => {
                 </div>
             </div>
             
-            <p className="path-line">See this code in the following path: src/App.js > <a href="https://github.com/LisCT/flatten-array/blob/master/src/App.js" target="_blank" rel="noopener noreferrer">line 13</a></p>
+            <p className="path-line">See this code in the following path: src/App.js > <a href="https://github.com/LisCT/flatten-array/blob/master/src/App.js" target="_blank" rel="noopener noreferrer">line 23</a></p>
 
         </div>
     )
